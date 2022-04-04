c51886f1dba5976eea4afb337eb266f22693da61
jojuaswand <jojuaswand10@gmail.com>
2022-04-04 10:02:06 +0530

    day 2

---
+++ basics/keywords.py
@@ -0,0 +1,14 @@
+# help("keywords")
+# import keyword #keyword is a module
+# # print(keyword.kwlist)
+# print(keyword.iskeyword("true"))
+# print(keyword.iskeyword("True"))
+# x = "hello"
+# print(keyword.iskeyword(x))
+# y = 10
+# print(keyword.iskeyword(y))
+
+
+#wap to give all the keywords in table and list format
+#wap to check whether help, hello, False,none is a keyword
+
--- basics/print()input().py
+++ basics/print()input().py
@@ -1,8 +1,37 @@
-x = 10
+# x = 10
+# y = 20
+# print(x)
+# print(y)
+# print(x,y,10,20)
+# print(x,y, sep=".", end="@") #default values sep = " " end = "\n"
+# print("hello", "world", sep="")
+# print(x, end=" ")
+# print(y, end="")
 
-y = 20
-print(x)
-print(y)
-print(x,y,10,20)
-print(x,y, sep=".", end="@") #default values sep = " " end = "\n"
-print("hello", "world", sep=",", end="/")
+# print(x, y, sep="#", end="#")
+# print(x, y, sep="@", end="#")
+# print(x, end="x")
+
+# print("hello",input())
+
+# y = "hello"
+# x = input()
+# print(y, x)
+# print("value for x")
+# x = input()
+# print("value for y")
+# y = input()
+# print(x, y)
+
+# x = input("give value for x: ")
+# print(x)
+
+# a = input("give a message: ")
+# x = input(a)
+# print(x)
+# x = input("give value: ")
+# print(x)
+
+#wap to print three variables separated by && and ending in with #
+#wap to print #helloxworldxbye#
+#wap to take two user input and separate them by *
