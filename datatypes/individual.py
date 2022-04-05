e05afc5a0ccfa8ddd6c58cc2eee1dff22739526f
jojuaswand <jojuaswand10@gmail.com>
2022-04-05 10:16:30 +0530

    data types day1

---
+++ datatypes/individual.py
@@ -0,0 +1,70 @@
+#integers range from 0 to infinity to -infinity
+x = 1
+print(type(x))
+y = 2
+# print(type(y))
+# print(x + y)
+# print(type(x + y))
+#arithmetic operations
+# add = x + y #3
+# sub = x - y #-1
+# div = x/y #.5
+# mul = x * y #2
+#
+# print(add, sub, div, mul)
+#
+# mod = 5 % x #0
+# print(mod)
+#
+# floordiv = 105 // 10 #10
+# print(floordiv)
+#
+# dm = divmod(115, 10)
+# print(dm)
+#
+# a = -5
+# print(type(a))
+#
+# print(abs(a))
+# print(abs(x))
+#
+#
+# print(dir(x))
+# print(dir(int))
+#creating integers
+# i = 10
+# r = int(100)
+# print(r)
+# r = int(0.5)
+# print(r)
+# r = int(-5)
+# print(r)
+# x = int()
+# print(x)
+# #float is integers with decimal point or decimal values 0.0 to infinity to -infinity
+# f = 0.5
+# print(type(f))
+# f = float(0)
+# print(f)
+# f = float(10.0)
+# print(f)
+# f = float(-1.5)
+# print(f)
+# print(abs(f))
+
+# 0.1 - 0.4 => 0 1.4, 1 , 1+0
+# 0.5 - 0.9 => 1 1.5, 2 , 1 + 1
+
+print(round(1.5))
+print(round(1.4))
+print(round(1.45))
+print(round(0.56))
+print(round(0.46))
+print(round(0.56, 1))
+print(round(0.46, 1))
+print(round(0.462, 2))
+print(round(1.562, 3))
+
+
+
+
