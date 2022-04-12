6e9707949f6b4b2f5f9dcdbeeef750cce4243e08
jojuaswand <jojuaswand10@gmail.com>
2022-04-12 09:19:52 +0530

    list day 2

--- datatypes/list.py
+++ datatypes/list.py
@@ -81,21 +81,134 @@ l.append("12") #it supports only one element of any type
 # print(l.count("hi"))
 
 #extend
-l = ["hi", "hello", "hi", "he", "llo"]
-l.extend("1") #same as append
-print(l)
-l.extend("2")
-print(l)
-l.extend([1, 2]) #it gives elements from the collection and extends the list
-print(l)
-l.append([1, 2]) #it takes the value itself without change and adds to the list
-print(l)
-l.extend([3, "hi"]) #values given should be in the form of collection
-print(l)
-l.extend(["12", 12]) #inside collection we can give any type of data
-print(l)
-l.extend("hello")
-print(l)
+# l = ["hi", "hello", "hi", "he", "llo"]
+# l.extend("1") #same as append
+# print(l)
+# l.extend("2")
+# print(l)
+# l.extend([1, 2]) #it gives elements from the collection and extends the list
+# print(l)
+# l.append([1, 2]) #it takes the value itself without change and adds to the list
+# print(l)
+# l.extend([3, "hi"]) #values given should be in the form of collection
+# print(l)
+# l.extend(["12", 12]) #inside collection we can give any type of data
+# print(l)
+# l.extend("hello")
+# print(l)
+#
+# #[1, 2, 3, 1 ,1, 1 , 5] wap to count 1 from the list and append the count of 1 to the original list
+# l = [1,1,1,1,2, 3 ,4 ,5 ,6]
+# l.append(l.count(1))
+# print(l)
 
+#index()
+l = [1, 1.2, 1+2j, "hello", "oh"]
+# print(l.index(1+2j))
+# # print(l.index("h")) Value error "h" element doesnt exist
+# print(l.index("hello"))
+# print(l[3].index("h"))
 
+# #isinstance
+# print(isinstance) #inbuilt function
+# print(isinstance(l, list)) #to check if  a value belongs to a certain data type
+# print(isinstance(l[3], list))
+# print(isinstance(l[3], (list, str))) #we can mention multiple data types and even if 1 of them is true it returns true
+# print(isinstance(l[0], (int, float, complex, str, list)))
 
+#insert
+
+l = [1, 2, 3, 4, 5, 6]
+# print(l)
+# a = l.insert(0, 5) #inserting a new value to specified index
+# print(a) #it returns none
+# print(l) #it modifies original value
+# l.insert(3, "middle")
+# print(l)
+# l.insert(0, l)
+# print(l)
+# print(l[0])
+# print(l[0][0])
+# [l, 5, 1,2 ,"middle", 3,4,5,6]   [l, 5, 1,2 ,"middle", 3,4,5,6]
+#  0   1  2 3   4       5 6 7 8     0   1  2 3   4       5 6 7 8
+
+#pop
+# l = [1, 2, 3, 4, 5, 6]
+# print(l.pop()) #pop removes a element from original list and it returns the popped item
+# print(l.pop(0)) #we can specify the index to pop, but by default it pops the last element
+# print(l)
+# l = ["hi", 1 , 2, 3]
+# # print(l[0].pop()) #AttributeError: 'str' object has no attribute 'pop'
+# # print(l.pop("h"))#only accepts index value
+# print(l)
+# l = [["hi"], 1]
+# print(l[0].pop(), l)
+
+#remove
+
+# l = [1, 2, 3, "hi", 4, 5, 6]
+# print(l.remove(1), l) #remove removes the specified value from original list and returns none and affects original list
+# l.remove("hi")
+# print(l)
+# l.remove("hi") ValueError: list.remove(x): x not in list if element not in list
+# print(l)
+
+#reverse
+# l = [1,2,5,3,4]
+# print(l[::-1])
+# print(l.reverse()) #it reverses the original list and returns none
+# print(l) #it wont change the values it just doese reverse
+# l.reverse()
+# # l.reverse(0) #it takes no value
+
+#sort
+l = [10, 5, 9, 4, 7, 2, 8, 1, 6, 3]
+# print(l)
+# print(l.sort()) #sort is sorting the list in ascending order by default and returns none and it affects original value
+# print(l)
+# l.sort(reverse= True) #sorted but in descending order because we have mentioned reverse is true
+#sorts the list and then reverses it
+# print(l)
+l = ["hi", "apple", "cat", "zebra"]
+# print(l)
+# l.sort() #for strings it is based on the 1st character by default and sorts bsed on alphabetical order
+# l.sort(reverse= True) #by default reverse will be false
+# l.sort(reverse= False, key= len) #key takes function names and based on that it sorts
+# l.sort(key=len) #key takes function names not function call
+# l.sort(reverse=True , key=len)
+# print(l)
+# l = ["2hi", "1apple", "4cat", "3zebra"]
+# l.sort()
+# print(l)
+
+#copy
+
+# l = [1, 2, 4, 3]
+# l1 = l #l1 is the same as l
+# print(l)
+# print(id(l))
+# print(l1)
+# print(id(l1))
+# l.sort()
+# print(l)  #if changes are made to l
+# print(l1) #it affects l1 too
+
+# l = [1, 2, 4, 3]
+# l1 = l.copy() #l1 is copying the value of l
+# print(l)
+# print(id(l))
+# print(l1)
+# print(id(l1)) #l and l1 have different addresses even when they have same value
+# print(l[0], id(l[0]))
+# print(l1[0], id(l1[0])) #address of list remains different but the elements will be having same object address
+# l.sort()
+# print(l) #changes made to l
+# print(l1) #are not affecting l1
+
+#
+# a = "hello"
+# b = a[::-1]
+# print(a, id(a))
+# print(b, id(b))
+# print(a[0], id(a[0]))
+# print(b[-1], id(b[-1]))
