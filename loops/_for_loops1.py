febf1e44d0b814064d7ce1422524579279b63b07
jojuaswand <jojuaswand10@gmail.com>
2022-04-21 08:57:51 +0530

    loops day 1

---
+++ loops/_for_loops1.py
@@ -0,0 +1,48 @@
+
+
+# Loop means repeating again and again
+#looping in an object
+# print(help("keywords"))
+#for is a keyword
+#for will be having its on code block
+# for i in (1,2,3):
+#     print(i)
+#
+# s = "hello"
+# for i in s: #i is a variable and assigned value to the string
+#     print(s) #string is printed inside loop
+    # print(i) #i was having value 1st h, for every iteration it was changing from h till o
+#collections which can be iterated through can be called iterables
+
+# for i in 5:TypeError: 'int' object is not iterable
+#     print(i)
+
+#iterating through a list and counting the number of iterations
+# count = 0
+# for i in [1, 2, "hi"]:
+#     # count = 0 #count is inside code block so everytime loop runs count will become
+#     print(i)
+#     count += 1 #count = count + 1 here updation of count was after the loops end
+# print(count)
+# print(len([1, 2, "hi"]))
+#for loop is iterating the same number of times as the length of a collection
+# count = 0
+# for i in [1, 2, "hi"]:
+#     count += 1
+#     print(i, count) #here updation of count was before loops end
+
+# for i in (1, 2, "hi"):
+#     print(i)
+
+# for i in {1, 2, "hi"}:
+#     print(i)
+
+# for i in {"name": "steve", "age": 23}:
+#     print(i)
+
+# for i in {"name": "steve", "age": 23}.values():
+#     print(i)
+
+
+# for i in {"name": "steve", "age": 23}.items():
+#     print(i)
