b480856f6232ac7eb4b47157f3db695a2e96c246
jojuaswand <jojuaswand10@gmail.com>
2022-04-22 08:31:56 +0530

    loops day 2

---
+++ loops/while_loop1_.py
@@ -0,0 +1,50 @@
+s = "hello world"
+i = 0
+# while s: #while a value is existing for s keep on printing
+#     print(s)
+#     s.replace(s[i], "")
+#     i += 1
+
+# while i < len(s): #working like for loop but assigning value to a variable for iteration not possible directly giving some condition
+    # print(s)
+    # i += 1
+#until condition becomes false its going to loop
+
+
+# count = 0
+# while count < 10:
+#     count += 1
+#
+# print(count)
+#
+
+# count = 0
+# while count < 10:
+#     print("hi", "hello")
+#     count += 1
+#
+
+# s = "hello world"
+# i = 0
+# s1 = ""
+# while i < len(s):
+#     s1 = s[i] + s1
+#     i += 1
+#
+# print(s1)
+
+s = "hello world"
+i = 0
+l = []
+while i <len(s):
+    l.append((i, s[i]))
+    i += 1
+print(l)
+
+
+
+
+
+
+
+
