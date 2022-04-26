51fdfb6956d60919a7367e5f2e40c3833a91fb36
jojuaswand <jojuaswand10@gmail.com>
2022-04-26 08:27:02 +0530

    comprehension day 2

--- basics/comprehesnions_/list_compre.py
+++ basics/comprehesnions_/list_compre.py
@@ -63,19 +63,59 @@ l1 = []
 
 # question 1
 # check even or odd in the range of 1 - 50 if it is even multiply by two or else power it and store these inside a new list
+leven = []
+lodd = []
+# for i in range(1, 51):
+#     if i % 2 == 0:
+#         leven.append(i)
+#     else:
+#         lodd.append(i)
+# leven = [i for i in range(1, 51) if i % 2 == 0]
+# lodd = [i for i in range(1, 51) if i % 2 == 1]
+# print(leven)
+# print(lodd)
+
 
 # question 2
-# l = ["python", "hello", "people", 1, "hi", "person", 2, "last", "personality"]
+l = ["python", "hello", "people", 1, "hi", "person", 2, "last", "personality"]
 # if the string starts with p append it to a new list
+l1 = []
+for i in l:
+    if isinstance(i, str):
+        if i[0] in "pP":
+            l1.append(i)
+# print(l1)
 
 # question 3
-# l = ["python", "hello", "people", 1, "hi", "person", 2, "last", "personality"]
+l = ["python", "hello", "people", 1, "hi", "person", 2, "last", "personality"]
 # in a new list append the elements with their index values
+l1 = []
+for i in enumerate(l):
+    l1.append(i)
+
+l1 = [i for i in enumerate(l)]
+# print(l1)
 
 # question 4
-# l = ["apple", "google", "yahoo", "gmail", "elephant"]
+l = ["apple", "google", "yahoo", "gmail", "elephant"]
 # append the names starting with vowels into new list
+l1 = []
+for i in l:
+    if i[0] in "aeiouAEIOU":
+        l1.append(i)
+l1 = [i for i in l if i[0] in "aeiouAEIOU"]
+# print(l1)
 
 # question 5
-# l = ["apple", "google", "yahoo", "gmail", "elephant"]
+l = ["apple", "google", "yahoo", "gmail", "elephant"]
 # create a new list with all the words starting with consonants reversed and words starting with vowels as same
+l1 = []
+for i in l:
+    if i[0] in "aeiouAEIOU":
+        l1.append(i)
+    else:
+        l1.append(i[::-1])
+
+l1 = [i if i[0] in "aeiouAEIOU" else i[::-1] for i in l]
+
+# print(l1)
