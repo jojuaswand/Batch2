d4a1660f0763efd434e10b90ba3d1f1173163e57
jojuaswand <jojuaswand10@gmail.com>
2022-05-02 09:00:41 +0530

    comprehension day 4

--- basics/comprehesnions_/dict_compre_.py
+++ basics/comprehesnions_/dict_compre_.py
@@ -7,4 +7,4 @@ s = "for comprehension"
 # can use {} and dict()
 # {return value in key : value form for loop}
 # {return value of if in key : value form for loop if statement}
-# {return value of if in key : value if statement else return of elese but only value form for loop }
+# {return value of if in key : value if statement else return of else but only value form for loop }
