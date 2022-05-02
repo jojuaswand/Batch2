fdae01643990caf0a9bc7fcf5780f7ca1c2bc976
jojuaswand <jojuaswand10@gmail.com>
2022-05-02 09:00:25 +0530

    comprehension day 4

---
+++ basics/comprehesnions_/dict_compre_.py
@@ -0,0 +1,10 @@
+s = "for comprehension"
+# d = {i: i for i in s}
+# print(d)
+# d = {i: v for i, v in enumerate(s)}
+# print(d)
+# syntax:
+# can use {} and dict()
+# {return value in key : value form for loop}
+# {return value of if in key : value form for loop if statement}
+# {return value of if in key : value if statement else return of elese but only value form for loop }
